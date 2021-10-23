ff937746920fddceb479b3c87c740a148444ef39
CKDEV_MAC2012 <9k.chainarong@gmail.com>
2021-10-23 20:49:39 +0700

    update

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,19 @@
+import { FaTimes } from "react-icons/fa";
+
 const Task = ({ task }) => {
   return (
     <div>
-      <h3>{task.text}</h3>
+      <h3>
+        {task.text} <FaTimes style={ timeStyle } />
+      </h3>
       <h3>{task.day}</h3>
     </div>
   );
+};
+
+const timeStyle = {
+  color: "red",
+  cursor: "pointer",
 };
 
 export default Task;
