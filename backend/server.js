324ffd693771910505d0540f539dc59f7195203f
msi <msi@msis-MacBook-Pro.local>
2021-11-17 23:41:41 +0700

    backend

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
